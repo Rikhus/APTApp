34f8c92073a776f54b4aeb8015808c7927d8c41d
Rikhus <no>
2020-12-31 19:34:10 +0300

    Replaced ListView for selecting course with buttons

--- app/src/main/java/com/example/aptapp/CourseSelectActivity.java
+++ app/src/main/java/com/example/aptapp/CourseSelectActivity.java
@@ -15,37 +15,27 @@ public class CourseSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_select);
-
-        ListView coursesList = findViewById(R.id.coursesList);
-
-        coursesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), GroupSelectActivity.class);
-
-                int selectedCourse = 0;
-
-                switch(parent.getItemAtPosition(position).toString()){
-                    case "1 курс":
-                        selectedCourse = 1;
-                        break;
-                    case "2 курс":
-                        selectedCourse = 2;
-                        break;
-                    case "3 курс":
-                        selectedCourse = 3;
-                        break;
-                    case "4 курс":
-                        selectedCourse = 4;
-                        break;
-                    default:
-                        Toast.makeText(CourseSelectActivity.this, "Выбран неверный курс", Toast.LENGTH_LONG).show();
-                        return;
-                }
-                intent.putExtra("selectedCourse", selectedCourse);
-                startActivity(intent);
-            }
-        });
     }
 
+    // нажатие кнопки выбора курса
+    public void selectCourseClick(View view) {
+        int selectedCourse = 0;
+        switch (view.getId()){
+            case (R.id.firstCourseButton):
+                selectedCourse = 1;
+                break;
+            case (R.id.secondCourseButton):
+                selectedCourse = 2;
+                break;
+            case (R.id.thirdCourseButton):
+                selectedCourse = 3;
+                break;
+            case (R.id.fourthCourseButton):
+                selectedCourse = 4;
+                break;
+        }
+        Intent intent = new Intent(getApplicationContext(), GroupSelectActivity.class);
+        intent.putExtra("selectedCourse", selectedCourse);
+        startActivity(intent);
+    }
 }
