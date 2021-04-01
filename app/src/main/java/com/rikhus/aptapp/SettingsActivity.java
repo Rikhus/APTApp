4b26e5c029451cf0706fcd7a89326fa3f9ae7225
Rikhus <no>
2021-04-02 00:35:00 +0300

    исправлен баг, при котором приложение вылетало на старых устройствах при выборе даты, исправлено расписание для преподавателей, сделан тестовый переключатель уведомлений а так же мелкие исправления

--- app/src/main/java/com/rikhus/aptapp/SettingsActivity.java
+++ app/src/main/java/com/rikhus/aptapp/SettingsActivity.java
@@ -1,20 +1,28 @@
 package com.rikhus.aptapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 
 import com.airbnb.paris.Paris;
+import com.rikhus.aptapp.ScheduleNotification.NewScheduleReleasedReciever;
 
 public class SettingsActivity extends ThemedActivity {
 
     Button lightThemeButton;
     Button darkThemeButton;
+    SwitchCompat switchScheduleNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,58 @@ public class SettingsActivity extends ThemedActivity {
                 Paris.styleBuilder(darkThemeButton).add(R.style.SettingsButtonSelected).apply();
                 break;
         }
+
+
+        // манипуляции со свитчем: если уведомления включены, то его тоже нужно включить
+        switchScheduleNotification = findViewById(R.id.switchScheduleNotification);
+        switchScheduleNotification.setChecked(getIsNotificationServiceEnabled());
+
+        // при изменении состояния свитча
+        switchScheduleNotification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    PackageManager pm  = getPackageManager();
+                    ComponentName componentName = new ComponentName(SettingsActivity.this, NewScheduleReleasedReciever.class);
+                    pm.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                            PackageManager.DONT_KILL_APP);
+                    setIsNotificationServiceEnabled(true);
+
+                    Intent intent = new Intent(SettingsActivity.this, NewScheduleReleasedReciever.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(SettingsActivity.this, 0, intent, 0);
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), Constants.CHECK_PERIOD_SECONDS * 1000, pendingIntent);
+
+                    setIsNotificationServiceStarted(true);
+                }
+                else{
+                    PackageManager pm  = getPackageManager();
+                    ComponentName componentName = new ComponentName(SettingsActivity.this, NewScheduleReleasedReciever.class);
+                    pm.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP);
+                    setIsNotificationServiceEnabled(false);
+                }
+            }
+        });
+    }
+
+    private void setIsNotificationServiceEnabled(Boolean isEnabled){
+        SharedPreferences preferences = getSharedPreferences("Notification", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("isEnabled", isEnabled);
+        editor.apply();
+    }
+
+    private void setIsNotificationServiceStarted(Boolean isStarted){
+        SharedPreferences preferences = getSharedPreferences("Notification", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("isStarted", isStarted);
+        editor.apply();
+    }
+
+    private boolean getIsNotificationServiceEnabled(){
+        SharedPreferences preferences = getSharedPreferences("Notification", Context.MODE_PRIVATE);
+        return  preferences.getBoolean("isEnabled", true);
     }
 
     public void selectTheme(View view) {
